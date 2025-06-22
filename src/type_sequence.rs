2d0453c7531d1e21fc50e99da76032a6356cdba3
orxfun <orx.ugur.arikan@gmail.com>
2025-06-22 21:39:33 +0200

    type sequence clean up

--- src/type_sequence.rs
+++ src/type_sequence.rs
@@ -1,8 +1,6 @@
 use std::marker::PhantomData;
 
 pub trait TypeSequence: Default {
-    type Next: TypeSequence;
-
     type ComposeWith<X>: TypeSequence;
 }
 
@@ -10,16 +8,12 @@ pub trait TypeSequence: Default {
 pub struct End;
 
 impl TypeSequence for End {
-    type Next = Self;
-
     type ComposeWith<X> = One<X>;
 }
 
 pub struct One<T>(PhantomData<T>);
 
 impl<T> TypeSequence for One<T> {
-    type Next = End;
-
     type ComposeWith<X> = Many<Self, One<X>>;
 }
 
@@ -31,22 +25,17 @@ impl<T> Default for One<T> {
 
 pub struct Many<T, U>(PhantomData<(T, U)>)
 where
-    T: TypeSequence,
     U: TypeSequence;
 
 impl<T, U> TypeSequence for Many<T, U>
 where
-    T: TypeSequence,
     U: TypeSequence,
 {
-    type Next = End;
-
     type ComposeWith<X> = Many<T, U::ComposeWith<X>>;
 }
 
 impl<T, U> Default for Many<T, U>
 where
-    T: TypeSequence,
     U: TypeSequence,
 {
     fn default() -> Self {
