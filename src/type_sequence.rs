6c988b4da85d82536e5153700a943fe9bd1c6690
orxfun <orx.ugur.arikan@gmail.com>
2025-06-22 21:38:11 +0200

    TypeSequence::ComposeWith

--- src/type_sequence.rs
+++ src/type_sequence.rs
@@ -1,38 +1,75 @@
 use std::marker::PhantomData;
 
-pub trait TypeSequence {
+pub trait TypeSequence: Default {
     type Next: TypeSequence;
 
     type ComposeWith<X>: TypeSequence;
 }
 
-pub struct TypeSequenceEnd {}
+#[derive(Default)]
+pub struct End;
 
-impl TypeSequence for TypeSequenceEnd {
+impl TypeSequence for End {
     type Next = Self;
 
-    type ComposeWith<X> = TypeSequenceOne<X>;
+    type ComposeWith<X> = One<X>;
 }
 
-pub struct TypeSequenceOne<T>(PhantomData<T>);
+pub struct One<T>(PhantomData<T>);
 
-impl<T> TypeSequence for TypeSequenceOne<T> {
-    type Next = TypeSequenceEnd;
+impl<T> TypeSequence for One<T> {
+    type Next = End;
 
-    type ComposeWith<X> = TypeSequenceMany<Self, TypeSequenceOne<X>>;
+    type ComposeWith<X> = Many<Self, One<X>>;
 }
 
-pub struct TypeSequenceMany<T, U>(PhantomData<(T, U)>)
+impl<T> Default for One<T> {
+    fn default() -> Self {
+        Self(Default::default())
+    }
+}
+
+pub struct Many<T, U>(PhantomData<(T, U)>)
 where
     T: TypeSequence,
     U: TypeSequence;
 
-impl<T, U> TypeSequence for TypeSequenceMany<T, U>
+impl<T, U> TypeSequence for Many<T, U>
 where
     T: TypeSequence,
     U: TypeSequence,
 {
-    type Next = TypeSequenceEnd;
+    type Next = End;
 
-    type ComposeWith<X> = TypeSequenceMany<T, U::ComposeWith<X>>;
+    type ComposeWith<X> = Many<T, U::ComposeWith<X>>;
+}
+
+impl<T, U> Default for Many<T, U>
+where
+    T: TypeSequence,
+    U: TypeSequence,
+{
+    fn default() -> Self {
+        Self(Default::default())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn abc() {
+        let x = End;
+        let y = compose_with::<_, u32>(x);
+        let z = compose_with::<_, char>(y);
+        let w = compose_with::<_, bool>(z);
+    }
+
+    fn compose_with<S, Y>(_: S) -> <S as TypeSequence>::ComposeWith<Y>
+    where
+        S: TypeSequence,
+    {
+        Default::default()
+    }
 }
