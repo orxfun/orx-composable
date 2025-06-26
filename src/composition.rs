f5488ff32f6f4e5550d1fec5d3bc0f2f996efec0
orxfun <orx.ugur.arikan@gmail.com>
2025-06-26 07:39:17 +0200

    Composition documentation and examples

--- src/composition.rs
+++ src/composition.rs
@@ -3,6 +3,83 @@ use crate::{Computation, ComputeReduce, Reduction, compute_reduce::ComputeReduce
 /// A reduction `R` and a reducible over `R` computation `C`, providing [`compose`] and [`compute`]
 /// methods.
 ///
+/// # Examples
+///
+/// The following example demonstrates creating a composition over the reduction `Add`.
+/// Then, this computation is composed with different computations requiring different
+/// inputs, while all returning a number.
+///
+/// ```
+/// struct Add;
+///
+/// impl Reduction for Add {
+///     type Unit = usize;
+///
+///     fn identity(&self) -> Self::Unit {
+///         0
+///     }
+///
+///     fn reduce(&self, a: Self::Unit, b: Self::Unit) -> Self::Unit {
+///         a + b
+///     }
+/// }
+///
+/// struct StrLen;
+///
+/// impl Computation for StrLen {
+///     type In<'i> = &'i str;
+///
+///     type Out = usize;
+///
+///     fn compute(&self, str: Self::In<'_>) -> Self::Out {
+///         str.len()
+///     }
+/// }
+///
+/// struct SliceLen;
+///
+/// impl Computation for SliceLen {
+///     type In<'i> = &'i [bool];
+///
+///     type Out = usize;
+///
+///     fn compute(&self, slice: Self::In<'_>) -> Self::Out {
+///         slice.len()
+///     }
+/// }
+///
+/// struct NumEvens;
+///
+/// impl Computation for NumEvens {
+///     type In<'i> = Vec<u64>;
+///
+///     type Out = usize;
+///
+///     fn compute(&self, vec: Self::In<'_>) -> Self::Out {
+///         vec.iter().filter(|x| *x % 2 == 0).count()
+///     }
+/// }
+///
+/// // compose 0
+/// let c = Composition::new(Add);
+/// assert_eq!(c.compute(()), 0);
+///
+/// // compose 1
+/// let c = Composition::new(Add).compose(StrLen);
+/// assert_eq!(c.compute("xyz"), 3);
+///
+/// // compose 2
+/// let c = Composition::new(Add).compose(StrLen).compose(SliceLen);
+/// assert_eq!(c.compute(("xyz", &[true, false])), 5);
+///
+/// // compose 3
+/// let c = Composition::new(Add)
+///     .compose(StrLen)
+///     .compose(SliceLen)
+///     .compose(NumEvens);
+/// assert_eq!(c.compute((("xyz", &[true, false]), vec![1, 2, 3, 4, 5])), 7);
+/// ```
+///
 /// [`compose`]: Composition::compose
 /// [`compute`]: Computation::compute
 pub struct Composition<R, C>
@@ -54,6 +131,84 @@ where
 
     type Out = R::Unit;
 
+    /// Computes output for the given `input`.
+    ///
+    /// # Examples
+    ///
+    /// The following example demonstrates creating a composition over the reduction `Add`.
+    /// Then, this computation is composed with different computations requiring different
+    /// inputs, while all returning a number.
+    ///
+    /// ```
+    /// struct Add;
+    ///
+    /// impl Reduction for Add {
+    ///     type Unit = usize;
+    ///
+    ///     fn identity(&self) -> Self::Unit {
+    ///         0
+    ///     }
+    ///
+    ///     fn reduce(&self, a: Self::Unit, b: Self::Unit) -> Self::Unit {
+    ///         a + b
+    ///     }
+    /// }
+    ///
+    /// struct StrLen;
+    ///
+    /// impl Computation for StrLen {
+    ///     type In<'i> = &'i str;
+    ///
+    ///     type Out = usize;
+    ///
+    ///     fn compute(&self, str: Self::In<'_>) -> Self::Out {
+    ///         str.len()
+    ///     }
+    /// }
+    ///
+    /// struct SliceLen;
+    ///
+    /// impl Computation for SliceLen {
+    ///     type In<'i> = &'i [bool];
+    ///
+    ///     type Out = usize;
+    ///
+    ///     fn compute(&self, slice: Self::In<'_>) -> Self::Out {
+    ///         slice.len()
+    ///     }
+    /// }
+    ///
+    /// struct NumEvens;
+    ///
+    /// impl Computation for NumEvens {
+    ///     type In<'i> = Vec<u64>;
+    ///
+    ///     type Out = usize;
+    ///
+    ///     fn compute(&self, vec: Self::In<'_>) -> Self::Out {
+    ///         vec.iter().filter(|x| *x % 2 == 0).count()
+    ///     }
+    /// }
+    ///
+    /// // compose 0
+    /// let c = Composition::new(Add);
+    /// assert_eq!(c.compute(()), 0);
+    ///
+    /// // compose 1
+    /// let c = Composition::new(Add).compose(StrLen);
+    /// assert_eq!(c.compute("xyz"), 3);
+    ///
+    /// // compose 2
+    /// let c = Composition::new(Add).compose(StrLen).compose(SliceLen);
+    /// assert_eq!(c.compute(("xyz", &[true, false])), 5);
+    ///
+    /// // compose 3
+    /// let c = Composition::new(Add)
+    ///     .compose(StrLen)
+    ///     .compose(SliceLen)
+    ///     .compose(NumEvens);
+    /// assert_eq!(c.compute((("xyz", &[true, false]), vec![1, 2, 3, 4, 5])), 7);
+    /// ```
     #[inline(always)]
     fn compute(&self, input: Self::In<'_>) -> Self::Out {
         self.computation.compute_reduce(&self.reduction, input)
--- src/computation.rs
+++ src/computation.rs
@@ -113,6 +113,106 @@ pub trait Computation {
 
     /// Computes output for the given `input`.
     ///
-    /// See [`Computation`] for examples.
+    /// # Examples
+    ///
+    /// The following example demonstrates an average age computation with
+    /// a single argument that is a collection of people.
+    ///
+    /// ```
+    /// use orx_composable::*;
+    ///
+    /// struct Person {
+    ///     age: u32,
+    /// }
+    ///
+    /// struct AverageAge;
+    ///
+    /// impl Computation for AverageAge {
+    ///     type In<'i> = &'i [Person];
+    ///
+    ///     type Out = u32;
+    ///
+    ///     fn compute(&self, people: Self::In<'_>) -> Self::Out {
+    ///         match people.len() {
+    ///             0 => 0,
+    ///             n => people.iter().map(|p| p.age).sum::<u32>() / n as u32,
+    ///         }
+    ///     }
+    /// }
+    ///
+    /// let average_age_computer = AverageAge;
+    ///
+    /// let people = [1, 26, 42, 99].map(|age| Person { age });
+    /// assert_eq!(average_age_computer.compute(&people), 42);
+    ///
+    /// let people = [].map(|age| Person { age });
+    /// assert_eq!(average_age_computer.compute(&people), 0);
+    /// ```
+    ///
+    /// As demonstrated below, the computation can capture and use internal immutable
+    /// data, `age_threshold` in this case.
+    ///
+    /// ```
+    /// use orx_composable::*;
+    ///
+    /// struct Person {
+    ///     age: u32,
+    /// }
+    ///
+    /// struct AverageAgeOfAdults {
+    ///     age_threshold: u32,
+    /// }
+    ///
+    /// impl Computation for AverageAgeOfAdults {
+    ///     type In<'i> = &'i [Person];
+    ///
+    ///     type Out = u32;
+    ///
+    ///     fn compute(&self, people: Self::In<'_>) -> Self::Out {
+    ///         let (count, sum) = people.iter().fold((0, 0), |(count, sum), person| {
+    ///             match person.age > self.age_threshold {
+    ///                 true => (count + 1, sum + person.age),
+    ///                 false => (count, sum),
+    ///             }
+    ///         });
+    ///         match count {
+    ///             0 => 0,
+    ///             n => sum / n as u32,
+    ///         }
+    ///     }
+    /// }
+    ///
+    /// let average_age_computer = AverageAgeOfAdults { age_threshold: 30 };
+    ///
+    /// let people = [1, 26, 42, 99].map(|age| Person { age });
+    /// assert_eq!(average_age_computer.compute(&people), 70);
+    ///
+    /// let people = [].map(|age| Person { age });
+    /// assert_eq!(average_age_computer.compute(&people), 0);
+    /// ```
+    ///
+    /// Finally, the following example defines `Append` as a computation
+    /// with two arguments a string slice and a character to append to it.
+    ///
+    /// ```
+    /// use orx_composable::*;
+    ///
+    /// struct Append;
+    ///
+    /// impl Computation for Append {
+    ///     type In<'i> = (&'i str, char);
+    ///
+    ///     type Out = String;
+    ///
+    ///     fn compute(&self, (str, c): Self::In<'_>) -> Self::Out {
+    ///         let mut str = str.to_string();
+    ///         str.push(c);
+    ///         str
+    ///     }
+    /// }
+    ///
+    /// let append = Append;
+    /// assert_eq!(append.compute((&"4", '2')), 42.to_string());
+    /// ```
     fn compute(&self, input: Self::In<'_>) -> Self::Out;
 }
