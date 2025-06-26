9fa453bea74262967a1ef79258af4e833273d37c
orxfun <orx.ugur.arikan@gmail.com>
2025-06-26 07:23:09 +0200

    composition constructor and composer are implemented

--- src/composition.rs
+++ src/composition.rs
@@ -1,9 +1,38 @@
-use crate::Reduction;
+use crate::{Computation, ComputeReduce, Reduction, compute_reduce::ComputeReduceEmpty};
 
 pub struct Composition<R, C>
 where
     R: Reduction,
+    C: ComputeReduce<R = R>,
 {
     reduction: R,
     computation: C,
+}
+
+impl<R> Composition<R, ComputeReduceEmpty<R>>
+where
+    R: Reduction,
+{
+    pub fn new(reduction: R) -> Self {
+        Self {
+            reduction,
+            computation: Default::default(),
+        }
+    }
+}
+
+impl<R, C> Composition<R, C>
+where
+    R: Reduction,
+    C: ComputeReduce<R = R>,
+{
+    pub fn compose<C2>(self, other: C2) -> Composition<R, C::Compose<C2>>
+    where
+        C2: Computation<Out = R::Unit>,
+    {
+        Composition {
+            reduction: self.reduction,
+            computation: self.computation.compose(other),
+        }
+    }
 }
